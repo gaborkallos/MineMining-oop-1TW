6d2048015ea7a725d60b7ee5dd52549f257d0d3a
gaborkallos <gaborkallos@gmail.com>
2018-11-20 14:12:43 +0100

    criterias added to input

--- MineSweeper.java
+++ MineSweeper.java
@@ -4,10 +4,19 @@ import java.lang.Math;
 public class MineSweeper {
     public static void main(String[] args) {
         Scanner readSize = new Scanner(System.in);
-        int size = getPlayerInput(readSize, "tablesize");
-        int numberOfMine = getPlayerInput(readSize, "number of mines");
+        int size;
+        do {
+            size = getPlayerInput(readSize, "tablesize (1-50)");
+        } while (size>50 || size<1);
+        int numberOfMine;
+        do {
+            numberOfMine = getPlayerInput(readSize, "number of mines (more than 0)");
+        } while (numberOfMine<=0);
         if (numberOfMine >= Math.pow(size, 2)){
             numberOfMine = (int)Math.pow(size, 2)-1;
+        }
+        if (size == 1){
+            numberOfMine = 1;
         }
         readSize.close();
         GameConfig game = new GameConfig(size, numberOfMine);

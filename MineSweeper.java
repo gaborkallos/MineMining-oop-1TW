1a6a2548aa217fd6b8598aa80f3e7cd776efccb6
zsombor <apathy.zsombor@gmail.com>
2018-11-22 08:59:45 +0100

    implemented lose conditional check

--- MineSweeper.java
+++ MineSweeper.java
@@ -7,10 +7,22 @@ public class MineSweeper {
         int[] parameters = getInputs();
         GameConfig game = new GameConfig(parameters[0], parameters[1]);
         int[][] gameTable = game.getMineTable();
-        PrintTable.printTable(gameTable);
-
+        UserShow ushow = new UserShow(parameters[0], gameTable);
+        char[][] showTable = ushow.getPrintTable();
+        printTable(gameTable);
+        System.out.println();
+        printCharTable(showTable);
         Input in = new Input();
         int[] firstMove = in.movement();
+        ushow.setMove(firstMove);
+        showTable = ushow.calculateShowTable();
+        if (checkLose(firstMove, gameTable)) {
+            System.out.println("Sorry you lose");
+        }
+        System.out.println();
+        printCharTable(showTable);
+        System.out.println(Arrays.toString(firstMove));
+
         scan.close();
     }
     private static int[] getInputs(){
@@ -38,5 +50,27 @@ public class MineSweeper {
         System.out.println("Please enter " + nameOfInput + "!");
         int size = scan.nextInt();
         return size;
+    }
+
+    public static void printTable(int[][] gameTable){
+        for (int[] line : gameTable){
+            System.out.println(Arrays.toString(line));
+        }
+    }
+
+    public static boolean checkLose(int[] move, int[][] table){
+        if (table[move[0]][move[1]] == 9) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public static void printCharTable(char[][] gameTable){
+        for (char[] line : gameTable){
+            System.out.println(Arrays.toString(line));
+        }
+
     }
 }
